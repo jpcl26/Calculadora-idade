3a4a4b90d805accc326bc3420078cc17f9cad44b
João Paulo Caldas Lucas <133928449+jpcl26@users.noreply.github.com>
2025-09-13 10:52:25 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { AgeCalculator } from './components/AgeCalculator';
+
+function App() {
+  return (
+    <main className="min-h-screen bg-off-white flex items-center justify-center p-4">
+      <AgeCalculator />
+    </main>
+  )
+}
+
+export default App
